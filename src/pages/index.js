da9df8e20792e405c9cd6f517e9e443520b19c13
Maral Najafi <marawlnajafi@gmail.com>
2023-04-18 18:20:01 +0330

    style: used tailwind utilities

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1>E-commerce website</h1>;
+  return (
+    <div className="flex flex-row justify-center items-center h-screen">
+      <h1>E-commerce website</h1>
+    </div>
+  );
 }
