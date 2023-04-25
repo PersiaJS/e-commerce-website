7255413ecb43afa5abef50d8ff9e73809c0d1226
Maral Najafi <marawlnajafi@gmail.com>
2023-04-25 16:43:47 +0330

    create: typography components

---
+++ src/components/Typography/H2.jsx
@@ -0,0 +1,3 @@
+export default function H2({ children }) {
+  return <h2 className="text-5xl font-bold text-black">{children}</h2>;
+}
---
+++ src/components/Typography/P.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function P({ children }) {
+  return <p className="text-1xl font-bold text-black">{children}</p>;
+}
