12eb823adb70fca58b0272e556361f78ea6ad4cd
Maral Najafi <marawlnajafi@gmail.com>
2023-04-29 21:40:51 +0330

    sample svg btn

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
+import Button from "@/components/Button/Button";
+import SVG from "@/components/SVG/SVG";
+
 export default function Home() {
   return (
     <div>
       <h1>E-commerce website</h1>
+      <Button>
+        <SVG id={"icon--x"} />
+      </Button>
     </div>
   );
 }
